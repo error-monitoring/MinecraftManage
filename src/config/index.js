667a99fe572e209cc76556330aefc56cfce657da
huangwenquan <wenquan.huang@magicwindow.cn>
2018-11-26 22:53:34 +0800

    add

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
-let baseApi = ''
+let urlBase = ''
 
 if(process.env.VUE_APP_EVENT == 'test'){
-    baseApi = 'http://47.98.132.24:7100'
+    urlBase = 'http://47.98.132.24:7100'
 }else{
-    baseApi = ''
+    urlBase = ''
 }
 
 
