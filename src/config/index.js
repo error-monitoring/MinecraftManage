aa6b3a5e5abc86b921e02d7f2d901ae5072ccb02
huangwenquan <wenquan.huang@magicwindow.cn>
2018-11-26 23:07:18 +0800

    add

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 let urlBase = ''
 
-if(process.env.VUE_APP_EVENT == 'test'){
-    urlBase = 'http://47.98.132.24:7100'
-}else{
-    urlBase = 'http://47.98.132.24:7100'
-}
+// if(process.env.VUE_APP_EVENT == 'test'){
+//     urlBase = 'http://47.98.132.24:7100'
+// }else{
+//     urlBase = 'http://47.98.132.24:7100'
+// }
 
 
 export {
